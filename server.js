d471ee3813d605aa1000a87ba1057b2c9bea848c
Tsitsimutsvedu <mutsvedutsitsi@gmail.com>
2025-11-16 13:13:26 +0200

    server fix

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
  * This server.js file is the primary file of the
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
 const express = require('express');
-const env = require('dotenv').config();
+require('dotenv').config();
 const app = express();
 const static = require('./routes/static');
 const expressLayouts = require('express-ejs-layouts');
@@ -25,13 +26,16 @@ app.set('layout', './layouts/layout'); // not at views root
  * Routes
  *************************/
 app.use(static);
+
 // Index route
 app.get('/', utilities.handleErrors(baseController.buildHome));
+
 // Inventory route
 app.use('/inv', inventoryRoute);
+
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
-	next({ status: 404, message: 'Sorry, we appear to have lost that page.' });
+  next({ status: 404, message: 'Sorry, we appear to have lost that page.' });
 });
 
 /* ***********************
@@ -39,32 +43,34 @@ app.use(async (req, res, next) => {
  * Place after all other middleware
  *************************/
 app.use(async (err, req, res, next) => {
-	let nav = await utilities.getNav();
-	console.error(`Error at: "${req.originalUrl}": ${err.message}`);
-	if (err.status == 404) {
-		message = err.message;
-	} else if(err.status === 500){
-		message = err.message;
-	} else{
-		message = ' Oh no! There was a crash. Maybe try a different route?';
-	}
-	res.render('errors/error', {
-		title: err.status || 'Server Error',
-		message,
-		nav,
-	});
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+
+  let message;
+  if (err.status === 404) {
+    message = err.message;
+  } else if (err.status === 500) {
+    message = err.message;
+  } else {
+    message = 'Oh no! There was a crash. Maybe try a different route?';
+  }
+
+  res.status(err.status || 500).render('errors/error', {
+    title: err.status || 'Server Error',
+    message,
+    nav,
+  });
 });
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT;
-const host = process.env.HOST;
+const port = process.env.PORT || 5500;
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-	console.log(`app listening on ${host}:${port}`);
+  console.log(`App listening on port ${port}`);
 });
