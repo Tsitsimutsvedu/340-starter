7aa59a47eea9329dcd665b8640a9492286aedd16
Tsitsimutsvedu <mutsvedutsitsi@gmail.com>
2025-12-11 15:58:12 +0200

    week 5 fixing errors

--- utilities/index.js
+++ utilities/index.js
@@ -11,9 +11,17 @@ Util.getNav = async function () {
   const data = await invModel.getClassifications();
   let list = '<ul>';
   list += '<li><a href="/" title="Home page">Home</a></li>';
+
   data.rows.forEach((row) => {
-    list += `<li><a href="/inv/type/${row.classification_id}" title="See our inventory of ${row.classification_name} vehicles">${row.classification_name}</a></li>`;
+    list += `
+      <li>
+        <a href="/inv/type/${row.classification_id}"
+           title="See our inventory of ${row.classification_name} vehicles">
+          ${row.classification_name}
+        </a>
+      </li>`;
   });
+
   list += '</ul>';
   return list;
 };
@@ -27,18 +35,29 @@ Util.buildClassificationGrid = async function (data) {
   }
 
   let grid = '<ul id="inv-display">';
+
   data.forEach((vehicle) => {
-    grid += `<li>
-      <a href="../../inv/details/${vehicle.inv_id}" title="View ${vehicle.inv_make} ${vehicle.inv_model} details">
-        <img src="${vehicle.inv_thumbnail}" alt="Image of ${vehicle.inv_make} ${vehicle.inv_model} on CSE Motors" />
-      </a>
-      <div class="namePrice">
-        <h2><a href="../../inv/details/${vehicle.inv_id}" title="View ${vehicle.inv_make} ${vehicle.inv_model} details">${vehicle.inv_make} ${vehicle.inv_model}</a></h2>
-        <span>$${new Intl.NumberFormat('en-US').format(vehicle.inv_price)}</span>
-        <hr />
-      </div>
-    </li>`;
+    grid += `
+      <li>
+        <a href="../../inv/details/${vehicle.inv_id}"
+           title="View ${vehicle.inv_make} ${vehicle.inv_model} details">
+          <img src="${vehicle.inv_thumbnail}"
+               alt="Image of ${vehicle.inv_make} ${vehicle.inv_model} on CSE Motors" />
+        </a>
+
+        <div class="namePrice">
+          <h2>
+            <a href="../../inv/details/${vehicle.inv_id}">
+              ${vehicle.inv_make} ${vehicle.inv_model}
+            </a>
+          </h2>
+
+          <span>$${new Intl.NumberFormat('en-US').format(vehicle.inv_price)}</span>
+          <hr />
+        </div>
+      </li>`;
   });
+
   grid += '</ul>';
   return grid;
 };
@@ -46,4 +65,44 @@ Util.buildClassificationGrid = async function (data) {
 /* ***************************************
  * Build inventory detail HTML
  **************************************** */
-Util.buildInvDe
+Util.buildInvDetail = async function (vehicle) {
+  if (!vehicle) {
+    return '<p class="notice">Vehicle not found.</p>';
+  }
+
+  return `
+    <section id="inv-detail">
+
+      <img src="${vehicle.inv_image}"
+           alt="Image of ${vehicle.inv_make} ${vehicle.inv_model}" />
+
+      <h2>${vehicle.inv_year} ${vehicle.inv_make} ${vehicle.inv_model}</h2>
+
+      <p class="price">
+        Price: $${new Intl.NumberFormat('en-US').format(vehicle.inv_price)}
+      </p>
+
+      <p><strong>Description:</strong> ${vehicle.inv_description}</p>
+      <p><strong>Color:</strong> ${vehicle.inv_color}</p>
+      <p><strong>Mileage:</strong>
+        ${new Intl.NumberFormat('en-US').format(vehicle.inv_miles)} miles
+      </p>
+
+    </section>
+  `;
+};
+
+/* ***************************************
+ * Error handler wrapper
+ **************************************** */
+Util.handleErrors = function (controller) {
+  return async function (req, res, next) {
+    try {
+      await controller(req, res, next);
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
+module.exports = Util;
